0b71f2734f3d651811a8468200403385909eaadd
Ivan Casado <icasadosar@gmail.com>
2022-06-23 20:23:04 +0200

    pruebas terraform 02

--- main.tf
+++ main.tf
@@ -14,11 +14,12 @@ resource "aws_vpc" "test-spot" {
 resource "aws_subnet" "subnet-test-spot" {
   # creates a subnet
   cidr_block        = "${cidrsubnet(aws_vpc.test-spot.cidr_block, 3, 1)}"
+  #cidr_block        = "10.0.1.0/24"
   vpc_id            = "${aws_vpc.test-spot.id}"
-  availability_zone = "eu-west-1"
+  availability_zone = "eu-west-1a"
 }
 
-resource "aws_security_group" "ingress-ssh" {
+resource "aws_security_group" "ingress-ssh-test" {
   name   = "allow-ssh-sg"
   vpc_id = "${aws_vpc.test-spot.id}"
 
@@ -113,15 +114,50 @@ resource "aws_key_pair" "spot_key" {
 }
 
 resource "aws_spot_instance_request" "test_worker" {
-  ami                    = "ami-66a7871c"
+  ami                    = "ami-0d71ea30463e0ff8d"
   spot_price             = "0.016"
-  instance_type          = "t3.small"
+  instance_type          = "t2.small"
   spot_type              = "one-time"
-  block_duration_minutes = "120"
+  #block_duration_minutes = "120"
   wait_for_fulfillment   = "true"
   key_name               = "spot_key"
+  #user_data              = "local.ec2_user_data" # no forces replacement
+  #user_data_replace_on_change = "false"
 
-  security_groups = ["${aws_security_group.ingress-ssh-test.id}", "${aws_security_group.ingress-http-test.id}",
+  lifecycle {
+    ignore_changes = [user_data]
+  }
+
+  # no forces replacement
+  vpc_security_group_ids = ["${aws_security_group.ingress-ssh-test.id}", "${aws_security_group.ingress-http-test.id}",
   "${aws_security_group.ingress-https-test.id}"]
   subnet_id = "${aws_subnet.subnet-test-spot.id}"
+
+  user_data = <<-EOF
+	      #!/bin/bash
+        sudo amazon-linux-extras enable nginx1
+        sudo yum clean metadata
+        sudo yum -y install nginx
+        sudo service nginx start
+        sudo mv /usr/share/nginx/html/ /usr/share/nginx/html_default
+        sudo mkdir /usr/share/nginx/html/
+        sudo chown nginx:nginx /usr/share/nginx/html/
+        sudo chmod 755 /usr/share/nginx/html/
+		    echo "Hello world from EC2 $(hostname -f) primera prueba terraform" > /usr/share/nginx/html/index.html
+        sudo chown nginx:nginx -R /usr/share/nginx/html/*
+        sudo chmod 644 /usr/share/nginx/html/*
+		    EOF
+
+  tags = {
+    Name = "ec2-test-ngnx-terraform"
+  }
+
+}
+
+output "instance_ip_public" {
+  value = "ssh -i ~/.ssh/id_rsa ec2-user@${aws_spot_instance_request.test_worker.public_ip}"
+}
+
+output "endpoint_https" {
+  value = "https://${aws_spot_instance_request.test_worker.public_ip}/index.html"
 }

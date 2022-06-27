209038e06d38b9b5c57f9037be2d41afc5d30a42
Ivan Casado <icasadosar@gmail.com>
2022-06-27 10:18:54 +0200

    prueba 2706-01

--- main.tf
+++ main.tf
@@ -182,7 +182,7 @@ resource "aws_eip" "ip-test-env" {
   vpc                       = true
   instance                  = "${aws_spot_instance_request.test_worker.spot_instance_id}"
   associate_with_private_ip = "10.0.1.10"
-  depends_on                = [aws_internet_gateway.test-env-gw]
+  depends_on                = [aws_spot_instance_request.test_worker]
 }
 
 output "instance_ip_public" {
